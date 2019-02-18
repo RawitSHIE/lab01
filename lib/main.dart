99a4235c09c889d528b0eaa57eca0ae959e34fb5
RawitSHIE <Rawitgun@gmail.com>
2019-02-18 11:21:12 +0700

    dropdown and selector

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       // home: MyHomePage(),
       initialRoute: "/",
       routes: {
-        "/": (context) => MyCustomForm(),
+        "/": (context) => FirstScreen(),
         "/second": (context) => SecondScreen(),
         // "/detail": (context) => DetailScreen(),
       },
--- lib/ui/first_screen.dart
+++ lib/ui/first_screen.dart
@@ -1,26 +1,79 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import './detail_screen.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+import 'package:intl/intl.dart';
+import 'package:image_picker/image_picker.dart';
 
-class FirstScreen extends StatelessWidget {
+class FirstScreen extends StatefulWidget {
+  @override
+  FirstScreenState createState() {
+    // TODO: implement createState
+    return FirstScreenState();
+  }
+}
+
+class FirstScreenState extends State<FirstScreen> {
+  List<String> _passengers = <String>['', '1', '2', '3', '4', '5', '6'];
+  String _passenger = "";
+  File _image;
+
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+    setState((){
+      _image = image;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera),
+        onPressed: (){
+          getImage();
+        },
+      ),
       appBar: AppBar(
         title: Text("First Screen"),
       ),
-      body: Center(
-          child: FlatButton(
-        child: Text("go to second screen"),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => DetailScreen(title: "hello"),
-                ),
-            );
-        },
-      )),
+      body: Column(
+        children: <Widget>[
+          _image == null? Text("None is selected") : Image.file(_image),
+          InputDecorator(
+            decoration: InputDecoration(
+              icon: Icon(Icons.people),
+              labelText: "Passenger",
+            ),
+            isEmpty: _passenger == "",
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton<String>(
+                value: _passenger,
+                isDense: true,
+                onChanged: (value) {
+                  setState(() {
+                    _passenger = value;
+                  });
+                },
+                items: _passengers.map((String value) {
+                  return DropdownMenuItem(
+                    child: Text(value),
+                    value: value,
+                  );
+                }).toList(),
+              ),
+            ),
+          ),
+          DateTimePickerFormField(
+            inputType: InputType.date,
+            format: DateFormat("yyy/MM/dd"),
+            decoration: InputDecoration(labelText: "Date"),
+          )
+        ],
+      ),
     );
   }
 }
